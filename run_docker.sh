9e00c274bd9b10628aacffa7ac874168c5bfd1d1
Michal Hynek <hynek.michal@gmail.com>
2021-07-06 07:02:32 -0700

    Task 2: Implemented run_docker.sh to build and run Dockerized app

--- run_docker.sh
+++ run_docker.sh
@@ -3,10 +3,10 @@
 ## Complete the following steps to get Docker running locally
 
 # Step 1:
-# Build image and add a descriptive tag
+docker build -t microservices-final-project .
 
 # Step 2: 
-# List docker images
+docker image ls
 
 # Step 3: 
-# Run flask app
+docker run --rm -p 8000:80 microservices-final-project
