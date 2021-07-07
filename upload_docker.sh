db2a7dd9d71f5f8d6ae208555afe620ca5bab758
Michal Hynek <hynek.michal@gmail.com>
2021-07-07 07:19:42 -0700

    Task 4: Implemented upload_docker.sh

--- upload_docker.sh
+++ upload_docker.sh
@@ -5,11 +5,14 @@
 
 # Step 1:
 # Create dockerpath
-# dockerpath=<your docker ID/path>
+dockerpath=michalhynek/microservices-final-project
 
 # Step 2:  
 # Authenticate & tag
 echo "Docker ID and Image: $dockerpath"
+cat dockerhub.key | docker login --username michalhynek --password-stdin
+docker tag microservices-final-project $dockerpath
 
 # Step 3:
 # Push image to a docker repository
+docker push $dockerpath
